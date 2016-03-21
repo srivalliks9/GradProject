189cf25ed6fe0cf6aba631dbf2874c96096478ec
Srivalli Devarakonda <srivalli.devarakonda@woodpeck3r.com>
2016-03-20 20:57:55 -0500

    Created a new project

---
+++ SecureAuth/SecureAuth/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SecureAuth.Startup))]
+namespace SecureAuth
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
